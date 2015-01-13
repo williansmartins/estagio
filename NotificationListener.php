ac32a0f71a49e0f7e2b75a97557640eb23244a41
will <professorwillians@gmail.com>
2015-01-13 00:51:17 -0200

    add integracao com o pagseguro e sandbox

---
+++ NotificationListener.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ ************************************************************************
+ Copyright [2014] [PagSeguro Internet Ltda.]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ************************************************************************
+ */
+
+require_once "PagSeguroLibrary/PagSeguroLibrary.php";
+
+class NotificationListener
+{
+
+    public static function main()
+    {
+
+        $code = (isset($_POST['notificationCode']) && trim($_POST['notificationCode']) !== "" ?
+            trim($_POST['notificationCode']) : null);
+        $type = (isset($_POST['notificationType']) && trim($_POST['notificationType']) !== "" ?
+            trim($_POST['notificationType']) : null);
+
+        echo ">>>".$code;
+        echo ">>".$type;
+
+        if ($code && $type) {
+            echo "sim";
+            $notificationType = new PagSeguroNotificationType($type);
+            $strType = $notificationType->getTypeFromValue();
+
+            switch ($strType) {
+
+                case 'TRANSACTION':
+                    self::transactionNotification($code);
+                    break;
+
+                default:
+                    LogPagSeguro::error("Unknown notification type [" . $notificationType->getValue() . "]");
+
+            }
+
+            self::printLog($strType);
+
+        } else {
+            echo "<<";
+            LogPagSeguro::error("Invalid notification parameters.");
+            self::printLog();
+
+        }
+
+    }
+
+    private static function transactionNotification($notificationCode)
+    {
+
+        /*
+         * #### Credentials #####
+         * Replace the parameters below with your credentials (e-mail and token)
+         * You can also get your credentials from a config file. See an example:
+         * $credentials = PagSeguroConfig::getAccountCredentials();
+         */
+        $credentials = new PagSeguroAccountCredentials("professorwillians@gmail.com",
+               "581746CFDE8F4E31B08B40DF82235BF3");
+        try {
+            $transaction = PagSeguroNotificationService::checkTransaction($credentials, $notificationCode);
+            // Do something with $transaction
+        } catch (PagSeguroServiceException $e) {
+            die($e->getMessage());
+        }
+
+    }
+
+    private static function printLog($strType = null)
+    {
+        $count = 5;
+        echo "<h2>Receive notifications</h2>";
+        if ($strType) {
+            echo "<h4>notifcationType: $strType</h4>";
+        }
+        echo "<p>Last <strong>$count</strong> items in <strong>log file:</strong></p><hr>";
+        echo LogPagSeguro::getHtml($count);
+    }
+}
+
+NotificationListener::main();
